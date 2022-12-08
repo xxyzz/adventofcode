3b635175ca9a6faffa124c6ea7c361ee55c105c6
xxyzz <gitpull@protonmail.com>
2022-12-08 19:14:09 +0800

    Day 7 Rust
    Simplify code, don't build a tree.

--- src/day7.rs
+++ src/day7.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs;
 
 pub fn resolve(part: u32) {
@@ -10,105 +11,61 @@ pub fn resolve(part: u32) {
     }
 }
 
-enum DirectoryContent {
-    File(NormalFile),
-    Dir(Directory),
-}
+fn add_file_sizes(text: String) -> Vec<u32> {
+    let mut pwd: Vec<&str> = vec![];
+    let mut dir_sizes: HashMap<String, u32> = HashMap::new();
 
-struct NormalFile {
-    name: String,
-    size: u32,
-}
-
-struct Directory {
-    name: String,
-    contents: Vec<DirectoryContent>,
-}
-
-fn create_fs_tree(text: String) -> Directory {
-    let mut pwd: Vec<&mut Directory> = vec![];
-    let mut root: Directory = Directory {
-        name: String::from("/"),
-        contents: vec![],
-    };
-    pwd.push(&mut root);
     for line in text.lines() {
         let output: Vec<&str> = line.split_whitespace().collect();
-        match output[0] {
-            "$" => {
-                if output[1] == "cd" {
-                    if output[2] == ".." {
-                        pwd.pop();
-                    } else if !pwd.is_empty() {
-                        for d in pwd.last().unwrap().contents.iter() {
-                            match d {
-                                DirectoryContent::Dir(dir) => {
-                                    if dir.name == output[2] {
-                                        pwd.push(dir);  // damn
-                                        break;
-                                    }
-                                }
-                                _ => (),
-                            }
-                        }
-                    }
-                }
+        if line.starts_with("$ cd") {
+            if line == "$ cd .." {
+                let current_dir = pwd.join("/");
+                pwd.pop();
+                let current_dir_size = *dir_sizes.get(&current_dir).unwrap();
+                let parent_dir = pwd.join("/");
+                dir_sizes
+                    .entry(String::from(parent_dir))
+                    .and_modify(|size| *size += current_dir_size);
+            } else {
+                pwd.push(output[2]);
+                let current_dir = pwd.join("/");
+                dir_sizes.insert(current_dir, 0);
             }
-            "dir" => {
-                pwd.last()
-                    .unwrap()
-                    .contents
-                    .push(DirectoryContent::Dir(Directory {
-                        name: String::from(output[1]),
-                        contents: vec![],
-                    }));
-            }
-            _ => {
-                pwd.last()
-                    .unwrap()
-                    .contents
-                    .push(DirectoryContent::File(NormalFile {
-                        name: String::from(output[1]),
-                        size: output[2].parse().unwrap(),
-                    }));
-            }
+        } else if !line.starts_with("$") && !line.starts_with("dir") {
+            let filesize: u32 = output[0].parse().unwrap();
+            let dir = pwd.join("/");
+            dir_sizes
+                .entry(String::from(dir))
+                .and_modify(|size| *size += filesize);
         }
     }
 
-    root
-}
-
-fn calculate_folder_size(folder: &Directory, folder_sizes: &mut Vec<u32>) -> u32 {
-    let mut size = 0;
-    for content in folder.contents.iter() {
-        match content {
-            DirectoryContent::File(f) => size += f.size,
-            DirectoryContent::Dir(d) => size += calculate_folder_size(d, folder_sizes),
-        }
+    for _ in 0..pwd.len() - 1 {
+        let current_dir = pwd.join("/");
+        pwd.pop();
+        let parent_dir = pwd.join("/");
+        let current_dir_size = *dir_sizes.get(&current_dir).unwrap();
+        dir_sizes
+            .entry(parent_dir)
+            .and_modify(|size| *size += current_dir_size);
     }
 
-    folder_sizes.push(size);
-    size
+    dir_sizes.into_values().collect()
 }
 
 fn part_one(text: String) -> u32 {
-    let root = create_fs_tree(text);
-    let mut folder_sizes: Vec<u32> = vec![];
-    calculate_folder_size(&root, &mut folder_sizes);
-
-    folder_sizes.iter().filter(|v| **v <= 100000).sum()
+    let dir_sizes = add_file_sizes(text);
+    dir_sizes.iter().filter(|v| **v <= 100000).sum()
 }
 
 fn part_two(text: String) -> u32 {
-    let root = create_fs_tree(text);
-    let mut folder_sizes: Vec<u32> = vec![];
-    calculate_folder_size(&root, &mut folder_sizes);
-    folder_sizes.sort();
+    let mut dir_sizes = add_file_sizes(text);
+    dir_sizes.sort();
     let total_disk_size = 70000000;
-    let root_size = folder_sizes.last().unwrap();
+    let root_size = *dir_sizes.last().unwrap();
     let free_size = total_disk_size - root_size;
-    for size in folder_sizes.iter() {
-        if size + free_size >= 30000000 {
+    for size in dir_sizes.iter() {
+        if *size + free_size >= 30000000 {
             return *size;
         }
     }
