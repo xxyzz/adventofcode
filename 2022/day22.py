fb8603002f84a1d074de86ca5aa8c5368cd79964
xxyzz <gitpull@protonmail.com>
2022-12-30 19:25:11 +0800

    Day 22 part 2 incorrect Python

--- 2022/day22.py
+++ 2022/day22.py
@@ -3,18 +3,23 @@ import unittest
 DAY = 22
 
 
-def parse_input(path: str) -> tuple[dict[tuple[int, int], str], list[int | str], int]:
+def parse_input(
+    path: str,
+) -> tuple[dict[tuple[int, int], str], list[int | str], int, int]:
     with open(path) as f:
         row = 0
         board = {}
         parsing_board = True
         path_str = ""
         start_column = -1
+        size = 100
         for line in f:
             if parsing_board:
                 if not line.strip():
                     parsing_board = False
                     continue
+                if len(line.strip()) < size:
+                    size = len(line.strip())
                 row += 1
                 for column, value in enumerate(line.rstrip(), start=1):
                     if value in [".", "#"]:
@@ -35,7 +40,7 @@ def parse_input(path: str) -> tuple[dict[tuple[int, int], str], list[int | str],
                 paths.append(c)
         if num:
             paths.append(int(num))
-        return board, paths, start_column
+        return board, paths, start_column, size
 
 
 DIRECTIONS = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # right, down, left, up
@@ -43,7 +48,7 @@ DIRECTIONS = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # right, down, left, up
 
 def part_one(input_path: str) -> int:
     row = 1
-    board, paths, column = parse_input(input_path)
+    board, paths, column, _ = parse_input(input_path)
     direction = 0
     for path in paths:
         match path:
@@ -63,7 +68,6 @@ def part_one(input_path: str) -> int:
             case "L":
                 direction = (direction - 1) % 4
 
-    # print(f"{row=} {column=} {direction=}")
     return 1000 * row + 4 * column + direction
 
 
@@ -78,8 +82,64 @@ def wrap_around(row, column, direction, board):
     return row, column
 
 
-def part_two(path: str) -> int:
-    pass
+def part_two(input_path: str) -> int:
+    row = 1
+    board, paths, column, size = parse_input(input_path)
+    direction = 0
+    for path in paths:
+        match path:
+            case int():
+                for _ in range(path):
+                    next_row = row + DIRECTIONS[direction][0]
+                    next_column = column + DIRECTIONS[direction][1]
+                    if (next_row, next_column) not in board:
+                        next_row, next_column, direction = wrap_around_cube(
+                            row, column, direction, board, size
+                        )
+                    if board[next_row, next_column] == "#":
+                        break
+                    row, column = next_row, next_column
+            case "R":
+                direction = (direction + 1) % 4
+            case "L":
+                direction = (direction - 1) % 4
+    return 1000 * row + 4 * column + direction
+
+
+# https://reddit.com/r/adventofcode/comments/zseg3c/_/j17qdop/
+# 0: right, 1: down, 2: left, 3:  up
+def wrap_around_cube(row, column, direction, board, size):
+    match (row - 1) // size, (column - 1) // size, direction:
+        # left edges
+        case 0, 1, 3:
+            return 3 * size + 1 + (column - 1) % size, 1, 0
+        case 3, 0, 2:
+            return 1, size + 1 + (row - 1) % size, 1
+        case 0, 1, 2:
+            return 2 * size + size - (row - 1) % size, 1, 0
+        case 2, 0, 2:
+            return size - (row - 1) % size, size + 1, 0
+        case 1, 1, 2:
+            return size * 2 + 1, 1 + (row - 1) % size, 1
+        case 2, 0, 3:
+            return size + 1 + (column - 1) % size, size + 1, 0
+        # right edges
+        case 0, 2, 3:
+            return size * 4, 1 + (column - 1) % size, 3
+        case 3, 0, 1:
+            return 1, size * 2 + 1 + (column - 1) % size, 1
+        case 0, 2, 0:
+            return size * 2 + size - (row - 1) % size, size * 2, 2
+        case 2, 1, 0:
+            return size - (row - 1) % size, size * 3, 2
+        case 0, 2, 1:
+            return size + 1 + (column - 1) % size, size * 2, 2
+        case 1, 1, 0:
+            return size, size * 2 + 1 + (row - 1) % size, 3
+        case 2, 1, 1:
+            return size * 3 + 1 + (column - 1) % size, size, 2
+        case 3, 0, 0:
+            return 3 * size, size + 1 + (row - 1) % size, 3
 
 
 class Test(unittest.TestCase):
@@ -87,9 +147,9 @@ class Test(unittest.TestCase):
         self.assertEqual(part_one(f"input/day{DAY}_test_input"), 6032)
 
     # def test_part_two(self):
-    #     self.assertEqual(part_two(f"input/day{DAY}_test_input"), )
+    #     self.assertEqual(part_two(f"input/day{DAY}_test_input"), 5031)
 
 
 if __name__ == "__main__":
     print(part_one(f"input/day{DAY}_input"))
-    # print(part_two(f"input/day{DAY}_input"))
+    print(part_two(f"input/day{DAY}_input"))
