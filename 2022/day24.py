af6439f4f6eef3acc802fa7abaa76d6cb80d49ea
xxyzz <gitpull@protonmail.com>
2022-12-31 09:47:33 +0800

    Day 24 part 2 Python

--- 2022/day24.py
+++ 2022/day24.py
@@ -57,22 +57,25 @@ def compute_new_blizzard_map(old_map, row_num, col_num):
 
 def part_one(input_path: str) -> int:
     init_blizzard_map, row_num, col_num, end = parse_map(input_path)
-    blizzard_maps = {0: init_blizzard_map}
+    return walk((0, 1), end, {0: init_blizzard_map}, row_num, col_num, 0)
+
+
+def walk(start, end, blizzard_maps, row_num, col_num, start_minutes):
     q: deque[tuple[tuple[int, int], int]] = deque()
-    q.append(((0, 1), 0))
+    q.append((start, start_minutes))
     visited = set()
     while q:
-        my_position, steps = q.popleft()
-        if (my_position, steps) in visited:
+        my_position, minutes = q.popleft()
+        if (my_position, minutes) in visited:
             continue
-        visited.add((my_position, steps))
-        if steps + 1 in blizzard_maps:
-            blizzard_map = blizzard_maps[steps + 1]
+        visited.add((my_position, minutes))
+        if minutes + 1 in blizzard_maps:
+            blizzard_map = blizzard_maps[minutes + 1]
         else:
             blizzard_map = compute_new_blizzard_map(
-                blizzard_maps[steps], row_num, col_num
+                blizzard_maps[minutes], row_num, col_num
             )
-            blizzard_maps[steps + 1] = blizzard_map
+            blizzard_maps[minutes + 1] = blizzard_map
 
         row, col = my_position
         for dr, dc in [(-1, 0), (0, 1), (1, 0), (0, -1), (0, 0)]:
@@ -84,46 +87,8 @@ def part_one(input_path: str) -> int:
                 and 0 <= new_col < col_num
             ):
                 if (new_row, new_col) == end:
-                    return steps + 1
-                q.append(((new_row, new_col), steps + 1))
-
-    return 0
-
-
-def test_blizzard(path):
-    blizzard_map, row_num, col_num, end = parse_map(path)
-    print(f"{row_num=} {col_num=} {end=}")
-    print_map(blizzard_map, row_num, col_num)
-    for _ in range(5):
-        new_blizzard_map = defaultdict(str)
-        for blizzard_loc, blizzard_directions in blizzard_map.items():
-            if blizzard_directions == "#":
-                new_blizzard_map[blizzard_loc] = "#"
-            else:
-                for blizzard_direction in blizzard_directions:
-                    blizzard_row, blizzard_col = blizzard_loc
-                    match blizzard_direction:
-                        case "^":
-                            new_blizzard_loc = blizzard_row - 1, blizzard_col
-                        case "v":
-                            new_blizzard_loc = blizzard_row + 1, blizzard_col
-                        case "<":
-                            new_blizzard_loc = blizzard_row, blizzard_col - 1
-                        case ">":
-                            new_blizzard_loc = blizzard_row, blizzard_col + 1
-                    if blizzard_map.get(new_blizzard_loc) == "#":
-                        match blizzard_direction:
-                            case "^":
-                                new_blizzard_loc = row_num - 2, blizzard_col
-                            case "v":
-                                new_blizzard_loc = 1, blizzard_col
-                            case "<":
-                                new_blizzard_loc = blizzard_row, col_num - 2
-                            case ">":
-                                new_blizzard_loc = blizzard_row, 1
-                    new_blizzard_map[new_blizzard_loc] += blizzard_direction
-        blizzard_map = new_blizzard_map
-        print_map(blizzard_map, row_num, col_num)
+                    return minutes + 1
+                q.append(((new_row, new_col), minutes + 1))
 
 
 def print_map(blizzard_map, row_num, col_num):
@@ -143,18 +108,27 @@ def print_map(blizzard_map, row_num, col_num):
 
 
 def part_two(path: str) -> int:
-    pass
+    init_blizzard_map, row_num, col_num, end_loc = parse_map(path)
+    total_minutes = 0
+    start_loc = (0, 1)
+    blizzard_maps = {0: init_blizzard_map}
+    for start, end in [
+        (start_loc, end_loc),
+        (end_loc, start_loc),
+        (start_loc, end_loc),
+    ]:
+        total_minutes = walk(start, end, blizzard_maps, row_num, col_num, total_minutes)
+    return total_minutes
 
 
 class Test(unittest.TestCase):
     def test_part_one(self):
-        # test_blizzard(f"input/day{DAY}_test_input1")
         self.assertEqual(part_one(f"input/day{DAY}_test_input"), 18)
 
-    # def test_part_two(self):
-    #     self.assertEqual(part_two(f"input/day{DAY}_test_input"), )
+    def test_part_two(self):
+        self.assertEqual(part_two(f"input/day{DAY}_test_input"), 54)
 
 
 if __name__ == "__main__":
     print(part_one(f"input/day{DAY}_input"))
-    # print(part_two(f"input/day{DAY}_input"))
+    print(part_two(f"input/day{DAY}_input"))
