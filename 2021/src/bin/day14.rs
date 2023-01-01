f50ee2de5eb84a36768f6a8791ae3338c4c5545e
xxyzz <gitpull@protonmail.com>
2023-01-01 16:49:29 +0800

    2021 day 14 part 2 Rust

--- 2021/src/bin/day14.rs
+++ 2021/src/bin/day14.rs
@@ -1,53 +1,92 @@
 use std::collections::HashMap;
 use std::fs;
 
-const DAY: u32 = 14;
+const DAY: u64 = 14;
 
-fn parse_input(text: &str) -> (Vec<char>, HashMap<&str, &str>) {
+// https://reddit.com/r/adventofcode/comments/rfzq6f/2021_day_14_solutions/hohc8vc
+fn parse_input(
+    text: &str,
+) -> (
+    HashMap<String, u64>,
+    HashMap<&str, char>,
+    HashMap<char, u64>,
+) {
     let mut template = "";
     let mut rules = HashMap::new();
     for line in text.lines() {
         if !line.is_empty() {
             if line.contains("->") {
                 let rule_values: Vec<&str> = line.split(" -> ").collect();
-                rules.insert(rule_values[0], rule_values[1]);
+                rules.insert(rule_values[0], rule_values[1].chars().last().unwrap());
             } else {
                 template = line;
             }
         }
     }
-    (template.chars().collect(), rules)
-}
-
-fn insert_polymer(template: Vec<char>, rules: &HashMap<&str, &str>) -> Vec<char> {
-    let mut new_template = template.clone();
+    let mut pair_counter: HashMap<String, u64> = HashMap::new();
     for index in 0..template.len() - 1 {
-        let pair = String::from_iter(template[index..index + 2].iter());
-        new_template.insert(2 * index + 1, rules[&pair[..]].chars().next().unwrap());
-    }
-    new_template
-}
-
-fn part_one(text: &str) -> u32 {
-    let (mut template, rules) = parse_input(text);
-    for _ in 0..10 {
-        template = insert_polymer(template, &rules);
-    }
-    let mut polymer_counter: HashMap<&char, u32> = HashMap::new();
-    for polymer in template.iter() {
-        polymer_counter
-            .entry(polymer)
+        pair_counter
+            .entry(template[index..index + 2].to_string())
             .and_modify(|count| *count += 1)
             .or_insert(1);
     }
-    let mut polymer_nums: Vec<&u32> = polymer_counter.values().collect();
+    let mut char_counter: HashMap<char, u64> = HashMap::new();
+    for c in template.chars() {
+        char_counter
+            .entry(c)
+            .and_modify(|counter| *counter += 1)
+            .or_insert(1);
+    }
+    (pair_counter, rules, char_counter)
+}
+
+fn insert_polymer(
+    pair_counter: &mut HashMap<String, u64>,
+    rules: &HashMap<&str, char>,
+    char_counter: &mut HashMap<char, u64>,
+) {
+    let pair_counter_clone = pair_counter.clone();
+    for pair in pair_counter_clone.keys() {
+        if rules.contains_key(pair.as_str()) {
+            let pair_count = pair_counter_clone.get(pair).unwrap();
+            pair_counter
+                .entry(pair.clone())
+                .and_modify(|count| *count -= pair_count);
+            let new_pair_a = format!("{}{}", pair.chars().nth(0).unwrap(), rules[pair.as_str()]);
+            let new_pair_b = format!("{}{}", rules[pair.as_str()], pair.chars().last().unwrap());
+            pair_counter
+                .entry(new_pair_a)
+                .and_modify(|count| *count += pair_count)
+                .or_insert(*pair_count);
+            pair_counter
+                .entry(new_pair_b)
+                .and_modify(|count| *count += pair_count)
+                .or_insert(*pair_count);
+            char_counter
+                .entry(rules[pair.as_str()])
+                .and_modify(|count| *count += pair_count)
+                .or_insert(*pair_count);
+        }
+    }
+}
+
+fn simulate(text: &str, steps: u64) -> u64 {
+    let (mut pair_counter, rules, mut char_counter) = parse_input(text);
+    for _ in 0..steps {
+        insert_polymer(&mut pair_counter, &rules, &mut char_counter);
+    }
+    let mut polymer_nums: Vec<&u64> = char_counter.values().collect();
     polymer_nums.sort();
     *polymer_nums.last().unwrap() - *polymer_nums[0]
 }
 
-// fn part_two(text: &str) -> u32 {
+fn part_one(text: &str) -> u64 {
+    simulate(text, 10)
+}
 
-// }
+fn part_two(text: &str) -> u64 {
+    simulate(text, 40)
+}
 
 #[cfg(test)]
 mod tests {
@@ -60,15 +99,16 @@ mod tests {
         assert_eq!(part_one(&lines), 1588);
     }
 
-    // #[test]
-    // fn test_part_two() {
-    //     let lines = fs::read_to_string("input/day{DAY}_test_input").expect("Can't read file");
-    //     assert_eq!(part_two(&lines), );
-    // }
+    #[test]
+    fn test_part_two() {
+        let lines =
+            fs::read_to_string(format!("input/day{DAY}_test_input")).expect("Can't read file");
+        assert_eq!(part_two(&lines), 2_188_189_693_529);
+    }
 }
 
 fn main() {
     let lines = fs::read_to_string(format!("input/day{DAY}_input")).expect("Can't read file");
     println!("{}", part_one(&lines));
-    // println!("{}", part_two(&lines));
+    println!("{}", part_two(&lines));
 }
