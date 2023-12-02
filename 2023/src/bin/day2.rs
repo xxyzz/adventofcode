301344bd7be286bc2acc8a82d42b3cbe70733d0a
xxyzz <gitpull@protonmail.com>
2023-12-02 13:50:56 +0800

    2023 day 2 part 2

--- 2023/src/bin/day2.rs
+++ 2023/src/bin/day2.rs
@@ -15,7 +15,7 @@ fn part_one(text: &str) -> u32 {
 
         for cube_sets in first_split[1].split("; ") {
             for cube_str in cube_sets.split(", ") {
-                let num_and_color: Vec<&str> = cube_str.split(" ").collect();
+                let num_and_color: Vec<&str> = cube_str.split(' ').collect();
                 let num: u32 = num_and_color[0].parse().unwrap();
                 let color = num_and_color[1];
                 if (color == "red" && num > 12)
@@ -34,8 +34,33 @@ fn part_one(text: &str) -> u32 {
     sum
 }
 
-// fn part_two(text: &str) -> u32 {
-// }
+fn part_two(text: &str) -> u32 {
+    let mut sum = 0;
+
+    for line in text.lines() {
+        let first_split: Vec<&str> = line.split(": ").collect();
+        let mut max_red = 0;
+        let mut max_green = 0;
+        let mut max_blue = 0;
+
+        for cube_sets in first_split[1].split("; ") {
+            for cube_str in cube_sets.split(", ") {
+                let num_and_color: Vec<&str> = cube_str.split(' ').collect();
+                let num: u32 = num_and_color[0].parse().unwrap();
+                let color = num_and_color[1];
+                if color == "red" && num > max_red {
+                    max_red = num;
+                } else if color == "green" && num > max_green {
+                    max_green = num;
+                } else if color == "blue" && num > max_blue {
+                    max_blue = num;
+                }
+            }
+        }
+        sum += max_red * max_green * max_blue;
+    }
+    sum
+}
 
 #[cfg(test)]
 mod tests {
@@ -52,14 +77,14 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
         assert_eq!(part_one(TEST_INPUT), 8);
     }
 
-    // #[test]
-    // fn test_part_two() {
-    //     assert_eq!(part_two(), 0);
-    // }
+    #[test]
+    fn test_part_two() {
+        assert_eq!(part_two(TEST_INPUT), 2286);
+    }
 }
 
 fn main() {
     let lines = fs::read_to_string("input/day2_input").expect("Can't read file");
     println!("{}", part_one(&lines));
-    // println!("{}", part_two(&lines));
+    println!("{}", part_two(&lines));
 }
