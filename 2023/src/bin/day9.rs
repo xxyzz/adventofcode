f92415a58d2931a6c0e43909981efb2838844232
xxyzz <gitpull@protonmail.com>
2023-12-12 09:16:59 +0800

    2023 day 9 part 1

---
+++ 2023/src/bin/day9.rs
@@ -0,0 +1,60 @@
+use std::fs;
+
+fn part_one(text: &str) -> i64 {
+    let histories: Vec<Vec<i64>> = text
+        .lines()
+        .map(|l| l.split_whitespace().map(|e| e.parse().unwrap()).collect())
+        .collect();
+    let mut predictions: Vec<i64> = vec![];
+    for history in &histories {
+        let mut diffs = vec![history.clone()];
+        loop {
+            let current_diff = diffs.last().unwrap();
+            let next_diff: Vec<i64> = current_diff
+                .windows(2)
+                .map(|window| window[1] - window[0])
+                .collect();
+            diffs.push(next_diff.clone());
+            if next_diff.iter().all(|&e| e == 0) {
+                break;
+            }
+        }
+        for i in (1..diffs.len()).rev() {
+            let a = *diffs[i - 1].last().unwrap();
+            let b = *diffs[i].last().unwrap();
+            diffs[i - 1].push(a + b);
+        }
+
+        predictions.push(*diffs[0].last().unwrap());
+    }
+
+    predictions.iter().sum()
+}
+
+// fn part_two(text: &str) -> u64 {
+// }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45";
+
+    #[test]
+    fn test_part_one() {
+        assert_eq!(part_one(TEST_INPUT), 114);
+    }
+
+    // #[test]
+    // fn test_part_two() {
+    //     assert_eq!(part_two(TEST_INPUT), );
+    // }
+}
+
+fn main() {
+    let lines = fs::read_to_string("input/day9").expect("Can't read file");
+    println!("{}", part_one(&lines));
+    // println!("{}", part_two(&lines));
+}
