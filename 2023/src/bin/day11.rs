c8d24a7fd33c6e91c6b8cdf04396b24a171f0512
xxyzz <gitpull@protonmail.com>
2023-12-19 20:31:55 +0800

    2023 day 11 part 2

--- 2023/src/bin/day11.rs
+++ 2023/src/bin/day11.rs
@@ -1,5 +1,5 @@
-use std::{fs, collections::HashSet};
 use std::cmp::{max, min};
+use std::fs;
 
 struct Point {
     row: i64,
@@ -11,55 +11,69 @@ impl Point {
         (self.row - p.row).abs() + (self.col - p.col).abs()
     }
 
-    fn distance(&self, p: &Point, empty_rows: &HashSet<i64>, empty_cols: &HashSet<i64>) -> i64 {
+    fn expanded_distance(
+        &self,
+        p: &Point,
+        empty_rows: &Vec<i64>,
+        empty_cols: &Vec<i64>,
+        expand: i64,
+    ) -> i64 {
         let mut distance = self.normal_distance(p);
         for empty_row in empty_rows {
             if *empty_row > min(self.row, p.row) && *empty_row < max(self.row, p.row) {
-                distance += 1;
+                distance += expand;
             }
         }
         for empty_col in empty_cols {
             if *empty_col > min(self.col, p.col) && *empty_col < max(self.col, p.col) {
-                distance += 1;
+                distance += expand;
             }
         }
         distance
     }
 }
 
-
-fn part_one(text: &str) -> i64 {
+fn solve(text: &str, expand: i64) -> i64 {
     let map: Vec<Vec<char>> = text.lines().map(|line| line.chars().collect()).collect();
-    let mut empty_rows: HashSet<i64> = HashSet::new();
-    let mut empty_cols: HashSet<i64> = HashSet::new();
+    let mut empty_rows: Vec<i64> = vec![];
+    let mut empty_cols: Vec<i64> = vec![];
     let mut galaxies: Vec<Point> = vec![];
     for (row_num, row) in map.iter().enumerate() {
         if row.iter().all(|e| *e == '.') {
-            empty_rows.insert(row_num as i64);
+            empty_rows.push(row_num as i64);
         }
         for (col_num, point_value) in row.iter().enumerate() {
             if *point_value == '#' {
-                galaxies.push(Point{row: row_num as i64, col: col_num as i64});
+                galaxies.push(Point {
+                    row: row_num as i64,
+                    col: col_num as i64,
+                });
             }
         }
     }
     for col in 0..map[0].len() {
         if (0..map.len()).all(|row| map[row][col] == '.') {
-            empty_cols.insert(col as i64);
+            empty_cols.push(col as i64);
         }
     }
 
     let mut results: i64 = 0;
     for i in 0..galaxies.len() - 1 {
-        for j in i+1..galaxies.len() {
-            results += galaxies[i].distance(&galaxies[j], &empty_rows, &empty_cols);
+        for j in i + 1..galaxies.len() {
+            results +=
+                galaxies[i].expanded_distance(&galaxies[j], &empty_rows, &empty_cols, expand);
         }
     }
     results
 }
 
-// fn part_two(text: &str) -> i64 {
-// }
+fn part_one(text: &str) -> i64 {
+    solve(text, 1)
+}
+
+fn part_two(text: &str) -> i64 {
+    solve(text, 999_999)
+}
 
 #[cfg(test)]
 mod tests {
@@ -80,15 +94,10 @@ mod tests {
     fn test_part_one() {
         assert_eq!(part_one(TEST_INPUT), 374);
     }
-
-    // #[test]
-    // fn test_part_two() {
-    //     assert_eq!(part_two);
-    // }
 }
 
 fn main() {
     let lines = fs::read_to_string("input/day11").expect("Can't read file");
     println!("{}", part_one(&lines));
-    // println!("{}", part_two(&lines));
+    println!("{}", part_two(&lines));
 }
