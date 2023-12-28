a2d5df0fcc03fcf90db904f3bf225d6cd95538be
xxyzz <gitpull@protonmail.com>
2023-12-28 22:03:44 +0800

    2023 day 15 part 2

--- 2023/src/bin/day15.rs
+++ 2023/src/bin/day15.rs
@@ -1,4 +1,8 @@
-use std::{fs, str::Chars};
+use std::{
+    collections::{HashMap, VecDeque},
+    fs,
+    str::Chars,
+};
 
 fn hash(chars: Chars) -> u64 {
     let mut current_value = 0;
@@ -17,8 +21,79 @@ fn part_one(text: &str) -> u64 {
     0
 }
 
-// fn part_two(text: &str) -> usize {
-// }
+struct LensHashMap {
+    len_boxes: HashMap<usize, VecDeque<(String, usize)>>,
+}
+
+impl LensHashMap {
+    fn process_step(&mut self, step: &str) {
+        if step.contains('=') {
+            let split_str: Vec<&str> = step.split('=').collect();
+            let marker = split_str[0];
+            let focal_length: usize = split_str[1].parse().unwrap();
+            self.insert(marker, focal_length);
+        } else if step.contains('-') {
+            let split_str: Vec<&str> = step.split('-').collect();
+            let marker = split_str[0];
+            self.remove(marker);
+        }
+    }
+
+    fn insert(&mut self, new_marker: &str, focal_len: usize) {
+        let box_index = hash(new_marker.chars()) as usize;
+        if !self.len_boxes.contains_key(&box_index) {
+            self.len_boxes.insert(box_index, VecDeque::new());
+        }
+        let mut inserted = false;
+        let lens = self.len_boxes.get_mut(&box_index).unwrap();
+        for (index, (marker, _)) in lens.clone().iter().enumerate() {
+            if *marker == new_marker {
+                inserted = true;
+                lens[index] = (new_marker.to_string(), focal_len);
+            }
+        }
+        if !inserted {
+            lens.push_back((new_marker.to_string(), focal_len));
+        }
+    }
+
+    fn remove(&mut self, new_marker: &str) {
+        let box_index = hash(new_marker.chars()) as usize;
+        if self.len_boxes.contains_key(&box_index) {
+            let lens = self.len_boxes.get_mut(&box_index).unwrap();
+            for (index, (marker, _)) in lens.clone().iter().enumerate() {
+                if *marker == new_marker {
+                    lens.remove(index);
+                }
+            }
+        }
+    }
+
+    fn compute_focus_power(&self) -> usize {
+        self.len_boxes
+            .iter()
+            .map(|(box_index, lens)| {
+                lens.iter()
+                    .enumerate()
+                    .map(|(len_index, (_, focal_len))| {
+                        (box_index + 1) * (len_index + 1) * focal_len
+                    })
+                    .sum::<usize>()
+            })
+            .sum()
+    }
+}
+
+fn part_two(text: &str) -> usize {
+    let mut len_hm = LensHashMap {
+        len_boxes: HashMap::new(),
+    };
+    if let Some(line) = text.lines().next() {
+        line.split(',').for_each(|step| len_hm.process_step(step));
+        return len_hm.compute_focus_power();
+    }
+    0
+}
 
 #[cfg(test)]
 mod tests {
@@ -31,14 +106,14 @@ mod tests {
         assert_eq!(part_one(TEST_INPUT), 1320);
     }
 
-    // #[test]
-    // fn test_part_two() {
-    //     assert_eq!(part_two(TEST_INPUT), );
-    // }
+    #[test]
+    fn test_part_two() {
+        assert_eq!(part_two(TEST_INPUT), 145);
+    }
 }
 
 fn main() {
     let lines = fs::read_to_string("input/day15").expect("Can't read file");
     println!("{}", part_one(&lines));
-    // println!("{}", part_two(&lines));
+    println!("{}", part_two(&lines));
 }
